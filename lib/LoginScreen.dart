0c86a6a4a8c2bf69ce96f89cd874a87f12e50c30
hussein <husseinhamada@agr.asu.edu.eg>
2022-04-08 16:27:27 +0200

    log in branch

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -9,7 +9,7 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
